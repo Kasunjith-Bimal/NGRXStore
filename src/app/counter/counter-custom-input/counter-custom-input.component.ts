26853f2b5af75b3cbdea1dd6eda21e4b116f73ea
Kasunjith Bimal Lakshitha <kasunjith@enadoc.com>
2024-04-13 09:39:49 +0530

    custom value passing store

--- src/app/counter/counter-custom-input/counter-custom-input.component.ts
+++ src/app/counter/counter-custom-input/counter-custom-input.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CounterState } from '../state/counter.state';
+import { Store } from '@ngrx/store';
+import { customIncrement } from '../state/counter.action';
 
 @Component({
   selector: 'app-counter-custom-input',
@@ -6,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./counter-custom-input.component.css']
 })
 export class CounterCustomInputComponent {
+ 
+  constructor(private store : Store<{counter:CounterState}>) {
+  }
+  value : number = 0;
 
+  OnAddCounter(){
+    this.store.dispatch(customIncrement({value : this.value}));
+  }
 }

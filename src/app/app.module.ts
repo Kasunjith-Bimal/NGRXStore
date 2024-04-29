e1196fd0975f3a8f4acb0fad18736fe5a5d497ec
Kasunjith-Bimal <kasunysoft@gmail.com>
2024-04-29 13:39:59 +0530

    lazzy loading counter and post with stormodule for feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,25 @@
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter/counter.component';
-import { CounterOutputComponent } from './counter/counter-output/counter-output.component';
-import { CounterButtonComponent } from './counter/counter-button/counter-button.component';
-import { CounterCustomInputComponent } from './counter/counter-custom-input/counter-custom-input.component';
 import { StoreModule } from '@ngrx/store';
-import { counterReducer } from './counter/state/counter.reducer';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './shared/components/header/header.component';
-import { PostListComponent } from './posts/post-list/post-list.component';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { appReducer } from './store/app.state';
-import { PostAddComponent } from './posts/post-add/post-add.component';
-import { PostEditComponent } from './posts/post-edit/post-edit.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent,
-    CounterOutputComponent,
-    CounterButtonComponent,
-    CounterCustomInputComponent,
     HomeComponent,
     HeaderComponent,
-    PostListComponent,
-    PostAddComponent,
-    PostEditComponent
+    
   ],
   imports: [
-    FormsModule,
-    ReactiveFormsModule,
     BrowserModule,
-    StoreModule.forRoot(appReducer),
+    StoreModule.forRoot({}),
     AppRoutingModule,
     StoreDevtoolsModule.instrument({
       logOnly: !isDevMode(), // Restrict extension to log-only mode
